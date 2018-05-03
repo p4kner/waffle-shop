88e1d432208cdf7d94c2ea338c099405d68d95ba
p4kner <p4kner@gmail.com>
2018-05-03 15:19:36 +0200

    Standard React State Change
    Implemented standard react state change in AppContainer component.
    A button which increases a counter.

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -2,24 +2,40 @@ import React, { Component } from 'react';
 import ReactNative from 'react-native';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
-import { ActionCreators} from '../actions'
+import { ActionCreators} from '../actions';
 
 const {
   View,
   Text,
+  TouchableHighlight,
 } = ReactNative;
 
 class AppContainer extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {recipeCount: 0};
+  }
+
+  incrementRecipeCount(){
+    this.setState({recipeCount: this.state.recipeCount + 1})
+  }
 
   render(){
     return(
       <View>
         <Text>
-          This is the AppContainer Component!
+          This is the AppContainer Component! Reciüpe cont: {this.state.recipeCount}
         </Text>
+        <TouchableHighlight onPress={() => {this.incrementRecipeCount()}}>
+          <Text>
+            Increase Count
+          </Text>
+        </TouchableHighlight>
       </View>
     )
   }
+
 }
 
 function mapDispatchToProps(dispatch){
